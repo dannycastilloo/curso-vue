048725e3692eea9d0d41b949969b4f9fc60f1600
dannycastilloo <dannycastillootiniano@gmail.com>
2023-12-23 20:57:32 -0500

    dynamic styles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <ul v-for="(message, index) in messages" :key='index' :class="mainClass">
-      <li>
+    <ul v-for="(message, index) in messages" :key='index'>
+      <li :class="{ active: linterna }">
         {{ index }} - {{ message }}
       </li>
     </ul>
     <button @click="addNewMessage">Agregar Mensaje</button>
-    
+
     <br><br>
 
     <h1 v-if="linterna">Encendido</h1>
     <h1 v-else>Apagado</h1>
     <button @click="turnOnOff">Encender/Apagar</button>
-    
+
     <br>
 
-    <h2 v-show="showName">Mi nombre es Danny</h2>
+    <h2 :class="{ active: showName }" :style="{ backgroundColor: showName ? 'red' : 'aquamarine' }">Mi nombre es Danny</h2>
     <button @click="changeShowStatus">Show Name</button>
     <br>
 
@@ -57,11 +57,14 @@ export default {
   },
   watch: {
     linterna(value, oldValue) {
-      console.log(`La linterna estaba en ${ oldValue } y ahora está en ${ value }`)
+      console.log(`La linterna estaba en ${oldValue} y ahora está en ${value}`)
     }
   }
 }
 </script>
 
 <style>
+.active {
+  color: green;
+}
 </style>
